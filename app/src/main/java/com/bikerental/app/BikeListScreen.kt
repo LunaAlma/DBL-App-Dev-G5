6871e7fcc805d2a2831243d70cd4c3033d74bc2d
hnnbr <hannabrita.kahari@gmail.com>
2025-03-10 10:15:28 +0100

    initialize BikeListScreen

---
+++ app/src/main/java/com/bikerental/app/BikeListScreen.kt
@@ -0,0 +1,18 @@
+package com.bikerental.app
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun BikeListScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp)
+    ) {
+        Text(text = "Available Bikes", style = MaterialTheme.typography.headlineMedium)
+    }
+}
